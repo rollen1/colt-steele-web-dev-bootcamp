5e9a72639212af9b4c3996b5f845f186d54f7af1
rollen1 <nolovedivision@gmail.com>
2022-09-13 01:27:14 +0300

    exercises

---
+++ src/js/15. our first variables/index.js
@@ -0,0 +1,12 @@
+/*
+
+In index.js, please define TWO variables:
+
+    Define a variable called myLuckyNumber and set it equal to 37.
+
+    Define a variable called octopusLimbs and set it equal to 8.
+
+*/
+
+let myLuckyNumber = 37;
+let octopusLimbs = 8;
