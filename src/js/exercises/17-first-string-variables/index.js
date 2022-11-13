8ebc8d4437069c7854878a8aea76b24467f0dd23
rollen1 <nolovedivision@gmail.com>
2022-11-13 16:04:17 +0200

    js strings and more

---
+++ src/js/exercises/17-first-string-variables/index.js
@@ -0,0 +1,12 @@
+/*
+
+Please define the following string variables (you can use either let or const):
+
+bestColor should be set to the string purple (the best color on planet earth, as we all know);
+
+quote should be set to the string You had me at "hello" - be careful about quotation marks, we want double quotes inside the string.
+
+*/
+
+const bestColor = "purple"
+const quote = 'you had me at "hello"'
