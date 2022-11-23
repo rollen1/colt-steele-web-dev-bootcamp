cfa5b927f0ae7c9901480a9271346531ff329d44
rollen1 <nolovedivision@gmail.com>
2022-11-23 22:40:49 +0200

    for loops

---
+++ src/js/19-loops/for-loops/app.js
@@ -0,0 +1,60 @@
+// for loop syntax
+
+/*
+
+for ({
+    initialExpression;
+    condition;
+    incrementExpression
+)};
+
+*/
+
+/*
+
+console.log(1)
+console.log(2)
+console.log(3)
+console.log(4)
+console.log(5)
+console.log(6)
+console.log(7)
+console.log(8)
+console.log(9)
+console.log(10)
+
+i++ (+1) until i = more or equal to 10
+
+for (let i = 1; i <= 10; i++) { 
+    console.log(i);
+}
+
+for (let num = 1; num <= 10, num + 1) {
+    console.log(num);
+}
+
+*/
+
+// more for loops
+
+/*
+
+// even numbers
+
+for (let i = 2; i <= 20; i+= 2) {
+    console.log(i);
+}
+
+// -10 from 100
+
+for (let i = 100; i >= 0; i -= 10) {
+    console.log(i);
+}
+
+// multiplication
+
+for (let i = 10; i <= 1000; i *= 10) {
+    console.log(i);
+}
+
+*/
---
+++ src/js/exercises/31-first-for-loop/index.js
@@ -0,0 +1,11 @@
+/*
+
+Please use a for loop to print out the classic chorus lyrics of "I'm Blue" by Eiffel 65.  Use a for loop to print out the string "Da ba dee da ba daa" exactly 6 times.  No more, and no less.
+
+*/
+
+// Print out "Da ba dee da ba daa" 6 times, using a for loop
+
+for (let imBlue = 1; imBlue <= 6; imBlue++ ) {
+    console.log("Da ba dee da ba daa");
+}
