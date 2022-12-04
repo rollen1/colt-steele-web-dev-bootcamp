3d3e6abeb2922fb5f46caa9e0f78f97044a039e7
rollen1 <nolovedivision@gmail.com>
2022-12-04 19:19:32 +0200

    to-do list

--- src/js/19-loops/to-do-list/app.js
+++ src/js/19-loops/to-do-list/app.js
@@ -1,6 +1,24 @@
-// for and a while loop
-// if else 
-// arrays with .splice
-
+let input = prompt('what would you like to do?');
 const todos = [];
-
+ 
+while (input !== 'quit' && input !== 'q') {
+    if (input === 'list') {
+        for (let i = 0; i < todos.length; i++) {
+            console.log(`${i}: ${todos[i]}`);
+        }
+    } else if (input === 'new') {
+        const newTodo = prompt('add a new task?');
+        todos.push(newTodo);
+        console.log(`${newTodo} added to the list!`)
+    } else if (input === 'delete') {
+        const index = parseInt(prompt('enter an index to delete'));
+        if (!Number.isNaN(index)) {
+            const deleted = todos.splice(index, 1);
+            console.log(`okay, deleted ${deleted[0]}`)
+        } else {
+            console.log('unknown index');
+        }
+    }
+    input = prompt('what would you like to do?');
+}
+console.log('quitting...');
